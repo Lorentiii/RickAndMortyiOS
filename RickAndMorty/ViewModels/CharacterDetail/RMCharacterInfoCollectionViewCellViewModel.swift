d62415fa1252de06aefc7a991ef0b9668ac7718b
Lorentiii <61249001+Lorentiii@users.noreply.github.com>
2023-03-07 20:03:54 -0500

    view cells

---
+++ RickAndMorty/ViewModels/CharacterDetail/RMCharacterInfoCollectionViewCellViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  RMCharacterInfoCollectionViewCellViewModel.swift
+//  RickAndMorty
+//
+//  Created by L's on 2023-03-07.
+//
+
+import Foundation
---
+++ RickAndMorty/Views/CharacterDetails/RMCharacterEpisodeCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  RMCharacterEpisodeCollectionViewCell.swift
+//  RickAndMorty
+//
+//  Created by L's on 2023-03-07.
+//
+
+import UIKit
+
+class RMCharacterEpisodeCollectionViewCell: UICollectionViewCell {
+    
+}
